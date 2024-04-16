64973094b512c2e6c838dcb936e69ce5d7391442
ushdu <dunuf>
2024-04-16 13:01:20 +0500

    setup db connection

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+const config  = require("../shared/config");
+const { Sequelize } = require("sequelize");
+
+const db = new Sequelize({
+  dialect: "postgres",
+  host: config.db.host,
+  port: config.db.port,
+  database: config.db.name,
+  username: config.db.username,
+  password: config.db.pwd,
+});
+
+module.exports = db;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const config = require("./shared/config");
-const apiRoutes = require("./api")
+const apiRoutes = require("./api");
+const db = require("./db");
 
 const app = express();
 
 app.use(apiRoutes);
 
-app.listen(config.port, () => {
-  console.log(`Server is listening on port ${config.port}`);
-});
+db.sync({ force: true })
+  .then(() => {
+    console.log("Connected to DB successfully");
+    
+    app.listen(config.port, () => {
+      console.log(`Server is listening on port ${config.port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error connecting to DB", err);
+  });
