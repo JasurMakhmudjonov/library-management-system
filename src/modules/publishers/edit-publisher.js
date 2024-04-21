2da98760b26035a2a71564ff21764861bd447300
JasurMakhmudjonov <153906711+JasurMakhmudjonov@users.noreply.github.com>
2024-04-21 13:20:11 +0500

    edit-publisher use-case

---
+++ src/modules/publishers/edit-publisher.js
@@ -0,0 +1,16 @@
+const { Publisher } = require("../../db/models");
+const { NotFoundError } = require("../../shared/errors");
+
+function editPublisher(id, newData) {
+  return Publisher.findByPk(id).then((publisher) => {
+    if (!publisher) {
+      throw new NotFoundError("Publisher is not found");
+    }
+
+    return publisher.update(data).then((updated) => {
+      return updated;
+    });
+  });
+}
+
+module.exports = editPublisher;
